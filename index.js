e70a5d939bc0b14dccc1b9508754bd6a40c5ae72
Gloria Jun <jiminisme@gmail.com>
2016-04-26 21:27:11 -0700

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+var myApp = angular.module('myApp', ['ngMap']);
+
+myApp.controller("TopSpots", [
+	'$scope', 
+	'$http', 
+	'NgMap', 
+	function($scope, $http, NgMap) {
+
+	var activate = function() {
+		$http.get('topSpots.json')
+				/*method: 'GET',
+				url: 'topSpots.json'
+			})*/
+			.then(function(response) {
+				$scope.topSpots = response.data;
+			});
+	};
+
+	$scope.addNewLocation = function (newTopSpot) {
+		if (isNaN(Number(newTopSpot.latitude))) {
+			alert("\"" + newTopSpot.longitude + "\"" + " is not a valid coordinate");
+			return;
+		}
+
+		if(isNaN(Number(newTopSpot.longitude))) {
+			alert("\"" + newTopSpot.longitude + "\"" + " is not a valid coordinate");
+			return;
+		}
+
+		newTopSpot.location = [Number(newTopSpot.latitude), Number(newTopSpot.longitude)];
+		$scope.topSpots.push(newTopSpot);
+
+	};
+
+	activate();
+}]);
